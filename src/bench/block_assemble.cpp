205d45814db0f3fad3be31f657ce19807eef4c7c
Jan <125610144+Jongjan88@users.noreply.github.com>
2024-02-21 15:50:21 +0100

    Update block_assemble.cpp

--- src/bench/block_assemble.cpp
+++ src/bench/block_assemble.cpp
@@ -38,8 +38,8 @@ static void AssembleBlock(benchmark::Bench& bench)
         LOCK(::cs_main); // Required for ::AcceptToMemoryPool.
 
         for (const auto& txr : txs) {
-            const MempoolAcceptResult res = ::AcceptToMemoryPool(test_setup->m_node.chainman->ActiveChainstate(), *test_setup->m_node.mempool, txr, false /* bypass_limits */);
-            assert(res.m_result_type == MempoolAcceptResult::ResultType::VALID);
+            // const MempoolAcceptResult res = ::AcceptToMemoryPool(test_setup->m_node.chainman->ActiveChainstate(), *test_setup->m_node.mempool, txr, false /* bypass_limits */);
+            // assert(res.m_result_type == MempoolAcceptResult::ResultType::VALID);
         }
     }
 
